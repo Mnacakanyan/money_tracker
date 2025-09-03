3c79540d37de937cfd8ca303c663003bf2b5a1d6
Mnacakanyan <n.mnacakanyan13@gmail.com>
2025-09-03 22:15:09 +0400

    Upgrade Android Gradle Plugin and clean up MainScreen

--- app/src/main/java/com/money/tracker/ui/screens/MainScreen.kt
+++ app/src/main/java/com/money/tracker/ui/screens/MainScreen.kt
@@ -8,11 +8,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.money.tracker.ui.theme.MoneyTrackerTheme
 
 @Composable
 fun MainScreen(navController: NavController) {

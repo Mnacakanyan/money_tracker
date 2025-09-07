ff934f7d2cb74d357e380490bf0bdfa7fd23aa1c
Mnacakanyan <n.mnacakanyan13@gmail.com>
2025-09-07 20:37:10 +0400

    Refactor navigation and update .gitignore

--- app/src/main/java/com/money/tracker/ui/MainActivity.kt
+++ app/src/main/java/com/money/tracker/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.money.tracker
+package com.money.tracker.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,23 +8,15 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.money.tracker.ui.MainViewModel
 import com.money.tracker.ui.navigation.Screen
-import com.money.tracker.ui.screens.CurrencySelectionScreen
-import com.money.tracker.ui.screens.GreetingScreen
-import com.money.tracker.ui.screens.MainScreen
-import com.money.tracker.ui.screens.NameInputScreen
+import com.money.tracker.ui.screens.AppNavigationHost
 import com.money.tracker.ui.theme.MoneyTrackerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -70,37 +62,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-        }
-
-    }
-}
-
-@Composable
-fun AppNavigationHost(
-    navController: NavHostController,
-    startDestination: String,
-    modifier: Modifier = Modifier,
-    mainViewModel: MainViewModel
-) {
-    NavHost(
-        navController = navController,
-        startDestination = startDestination,
-        modifier = modifier
-    ) {
-        composable(Screen.Greeting.route) {
-            GreetingScreen(
-                navController = navController,
-                onGreetingComplete = { mainViewModel.setGreetingCompleted(true) }
-            )
-        }
-        composable(Screen.NameInput.route) {
-            NameInputScreen(navController = navController)
-        }
-        composable(Screen.CurrencySelection.route) {
-            CurrencySelectionScreen(navController = navController)
-        }
-        composable(Screen.Main.route) {
-            MainScreen(navController = navController)
         }
     }
 }
---
+++ app/src/main/java/com/money/tracker/ui/screens/AppNavigationHost.kt
@@ -0,0 +1,39 @@
+package com.money.tracker.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.money.tracker.ui.MainViewModel
+import com.money.tracker.ui.navigation.Screen
+
+@Composable
+fun AppNavigationHost(
+    navController: NavHostController,
+    startDestination: String,
+    modifier: Modifier = Modifier,
+    mainViewModel: MainViewModel
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(Screen.Greeting.route) {
+            GreetingScreen(
+                navController = navController,
+                onGreetingComplete = { mainViewModel.setGreetingCompleted(true) }
+            )
+        }
+        composable(Screen.NameInput.route) {
+            NameInputScreen(navController = navController)
+        }
+        composable(Screen.CurrencySelection.route) {
+            CurrencySelectionScreen(navController = navController)
+        }
+        composable(Screen.Main.route) {
+            MainScreen(navController = navController)
+        }
+    }
+}
